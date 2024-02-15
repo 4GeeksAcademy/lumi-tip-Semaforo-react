d0f4a0c30a643edddf4c4ec5a922e438bf92fcb5
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-02-15 04:13:58 +0000

    :tada: Traffic Light Finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,62 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import { useEffect } from 'react';
 
 //create your first component
-const Home = () => {
+
+const LightBall = (props) =>{
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className={`light-ball ${props.lightBallColor} ${props.shine}`} onClick={props.handleClick}></div>
+	)
+}
+
+const Home = () => {
+	const [color, setColor] = useState("red");
+	const [auto, setAuto] = useState(false)
+
+	useEffect(()=>{
+		if(auto){
+			if(color === "red"){
+				setTimeout(()=>{
+					setColor("green")
+				}, 6000)
+			}
+			else if(color === "green"){
+				setTimeout(()=>{
+					setColor("yellow")
+				}, 5000)
+			}
+			else if(color === "yellow"){
+				setTimeout(()=>{
+					setColor("red")
+				}, 3000)
+			}
+		}
+	},[color, auto])
+
+	return (
+		<>
+			<div className="support"></div>
+			<div className="traffic-light">
+				<LightBall 
+					lightBallColor="red"
+					shine={color === "red" ? "shineRed" : undefined}
+					handleClick={()=> auto ? undefined : setColor("red")}
+				/>
+				<LightBall 
+					lightBallColor="yellow"
+					shine={color === "yellow" ? "shineYellow" : undefined}
+					handleClick={()=> auto ? undefined : setColor("yellow")}
+				/>
+				<LightBall 
+					lightBallColor="green" 
+					shine={color === "green" ? "shineGreen" : undefined} 
+					handleClick={()=> auto ? undefined : setColor("green")}
+				/>
+			</div>
+			{!auto && <button onClick={()=> setAuto(true)}>Automatico</button>}
+			{auto && <button onClick={()=> setAuto(false)}>Manual</button>}
+
+		</>
 	);
 };
 
